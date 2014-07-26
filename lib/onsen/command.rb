bbbaf524135cc444e87ad4f51fb9306d21f46a6d
Kosuke ENOMOTO <coskenomoto@gmail.com>
2014-07-27 03:27:59 +0900

    Modified command.rb

--- lib/onsen/command.rb
+++ lib/onsen/command.rb
@@ -7,29 +7,9 @@ require 'thor/group'
 
 module Onsen
   class Command < Thor
-<<<<<<< HEAD
-    option :message, :type => :boolean
-    option :test, :type => :boolean
-    desc "twitter times", "puts times twite"
-    def twitter(times)
-      output = []
-      output << "#{times} ツイートするよ？"
-      output << 'メッセージ追加です！！' if options[:message]
-      puts options[:test] ? (output << 'test実行です。') : output
-    end
-
-    ## July 26th Added by coosuke
-    desc "weather city", "get one city's weather forecast"
-    def weather(city, year, month, date)
-      output = []
-      output << "#{year} 年 #{month} 月 #{date} 日の #{city} の天気予報をお送りします"
-      puts output
-    end
-=======
     register(Twitter, 'twitter', 'twitter [COMMAND]', 'commands for twitter')
     register(Yo, 'yo', 'yo [COMMAND]', 'commands for yo')
     register(Weather, 'weather', 'weather [COMMAND]', 'commands for weather')
->>>>>>> develop
   end
 end
 
