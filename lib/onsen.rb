cc6f6c694779ad47c7c03d682d3e01f11a074dae
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2014-07-26 19:05:46 +0000

    fix yo client

--- lib/onsen.rb
+++ lib/onsen.rb
@@ -5,6 +5,13 @@ require "yaml"
 module Onsen
   # Your code goes here...
   def self.config
-    YAML.load_file(File.expand_path("~") + '/.onsenconfig')
+    file_path = File.expand_path("~") + '/.onsenconfig'
+    unless File.exist?(file_path)
+      temp = {'YO' => {'API_TOKEN' => 'SET_YOUR_TOKEN'}}
+      File.open(file_path, "w") do |f|
+        f.write(YAML.dump(temp))
+      end
+    end
+    YAML.load_file(file_path)
   end
 end
--- lib/onsen/weather.rb
+++ lib/onsen/weather.rb
@@ -1,7 +1,6 @@
 require 'onsen'
 require 'thor'
 require 'thor/group'
-require 'weatherhacks'
 
 module Onsen
   class Weather < Thor
@@ -9,11 +8,6 @@ module Onsen
     desc "send", "weather forcast for Tokyo today"
     def send
       puts "Get weather forcast for Tokyo today..."
-      day =:today
-      day_string = "今日"
-      forcast = weatherhacks.lwws("東京", day)
-      puts day_string + "の東京の天気"
-      puts forcast
     end
 
     def self.banner(task, namespace = false, subcommand = true)
