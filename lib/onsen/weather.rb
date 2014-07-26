aaddc3f3e99348e81f4c058febd4de1f76b6c2af
Kosuke ENOMOTO <coskenomoto@gmail.com>
2014-07-27 03:06:02 +0900

    Modified command.rb & weather.rb by coosuke

--- lib/onsen/weather.rb
+++ lib/onsen/weather.rb
@@ -1,6 +1,7 @@
 require 'onsen'
 require 'thor'
 require 'thor/group'
+require 'weatherhacks'
 
 module Onsen
   class Weather < Thor
@@ -8,6 +9,11 @@ module Onsen
     desc "send", "weather forcast for Tokyo today"
     def send
       puts "Get weather forcast for Tokyo today..."
+      day =:today
+      day_string = "今日"
+      forcast = weatherhacks.lwws("東京", day)
+      puts day_string + "の東京の天気"
+      puts forcast
     end
 
     def self.banner(task, namespace = false, subcommand = true)
