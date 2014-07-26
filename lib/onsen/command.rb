6d99edcf8a94305c78c7c49cbe5a6e7d57a06243
Kosuke ENOMOTO <coskenomoto@gmail.com>
2014-07-27 02:08:27 +0900

    Add weather.rb by coosuke
    weather.rb
      Created
    
    command.rb
      line 11: register(Weather, 'weather', 'weather', 'weather forcast fot
    Tokyo today')

--- lib/onsen/command.rb
+++ lib/onsen/command.rb
@@ -8,6 +8,7 @@ module Onsen
   class Command < Thor
     register(Twitter, 'twitter', 'twitter [COMMAND]', 'commands for twitter')
     register(Yo, 'yo', 'yo [COMMAND]', 'commands for yo')
+    register(Weather, 'weather', 'weather', 'weather forcast fot Tokyo today')
   end
 end
 
---
+++ lib/onsen/weather.rb
@@ -0,0 +1,17 @@
+require 'onsen'
+require 'thor'
+require 'thor/group'
+
+module Onsen
+  class weather < Thor
+    namespace :weather
+    desc "weather", "weather forcast fot Tokyo today"
+    def send
+      puts "Weather forcast for Tokyo today"
+    end
+
+    def self.banner(task, namespace = false, subcommand = true)
+      super
+    end
+  end
+end
